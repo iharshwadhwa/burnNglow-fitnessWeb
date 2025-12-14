c2d7a01ff256535d50e75e816ca58cc5b42d486d
Harsh Wadhwa <connect.harshwadhwa@gmail.com>
2025-12-14 21:52:29 +0530

    Use env vars for DB config

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10
 });
+
 
 // After ensuring the database exists, now connect with the database
 
